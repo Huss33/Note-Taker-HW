19ae3f8922cefd2d3a473a8ae7bab14fff245983
Bear Ridge <ahuss2010@live.com>
2022-01-24 09:51:22 -0600

    first commit

---
+++ helpers/fsUtils.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const util = require('util');
+
+const readDataFile = util.promisify(fs.readFile);
+
+const writeInFile = (destination, content) =>
+    fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+        err ? console.error(err) : console.info(`\nData written to ${destination}`));
+
+const readCopyAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const jParseData = JSON.parse(data);
+            jParseData.push(content);
+            writeInFile(file, jParseData);
+        }
+    });
+};
+
+module.exports = {readDataFile, writeInFile, readCopyAppend };
---
+++ routes/notes.js
@@ -0,0 +1,28 @@
+const notes = require('express').Router();
+const { readDataFile, readCopyAppend } = require('../helpers/fsUtils');
+const { v4: uuidv4 } = require('uuid');
+
+notes.get('/', (req, res) => {
+    readDataFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
+});
+
+notes.post('/', (req, res) => {
+    console.log(req.body);
+
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuidv4()
+        };
+
+        readCopyAppend(newNote, './db/notes.json');
+        res.json(`Note added successfully!`);
+    } else {
+        res.error(`Error in adding note!`);
+    }
+});
+
+module.exports = notes;
